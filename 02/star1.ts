088c345f670aca0cf74aebe3cd14322d56e6f330
Max B <m.bergmann@students.htl-leonding.ac.at>
2023-12-02 07:31:57 +0100

    added day02

--- 02/star1.ts
+++ 02/star1.ts
@@ -1,3 +1,100 @@
 import {getLines} from "../readFile";
 
-const lines = getLines("02/sample1.txt");
+const lines = getLines("02/input.txt");
+
+interface IGame {
+    id: number,
+    bags: IBag[]
+}
+
+interface IBag {
+    cubes: ICube[]
+}
+
+interface ICube {
+    amount: number,
+    color: string
+}
+
+let limit1: ICube = {
+    amount: 12,
+    color: "red"
+}
+
+let limit2: ICube = {
+    amount: 13,
+    color: "green"
+}
+
+let limit3: ICube = {
+    amount: 14,
+    color: "blue"
+}
+
+let games: IGame[] = [];
+
+for(let line of lines) {
+    line = line.replace("Game", "");
+    const vals: string[] = line.split(":");
+
+    const sets = vals[1].split(";")
+
+    const bags: IBag[] = [];
+
+    for(const set of sets) {
+        let cubesRaw: string[] = set.split(",");
+        const cubes: ICube[] = [];
+        for(const cubeRaw of cubesRaw) {
+            const data = cubeRaw.trim().split(" ");
+            const cube: ICube = {
+                amount: parseInt(data[0]),
+                color: data[1]
+            }
+
+            cubes.push(cube);
+        }
+
+        const bag: IBag = {
+            cubes: cubes
+        }
+
+        bags.push(bag)
+    }
+
+    let game: IGame = {
+        id: parseInt(vals[0].trim()),
+        bags: bags
+    }
+
+    games.push(game);
+}
+
+let gameIds: number[] = []
+
+for(let game of games) {
+    let bags: IBag[] = game.bags;
+    let check: boolean = false;
+
+    for(let bag of bags) {
+        for(let cube of bag.cubes) {
+            if ((cube.amount > limit1.amount && cube.color === limit1.color)
+                || (cube.amount > limit2.amount && cube.color === limit2.color)
+                || (cube.amount > limit3.amount && cube.color === limit3.color)) {
+                check = true;
+                break;
+            }
+        }
+    }
+
+    if(!check) {
+        gameIds.push(game.id);
+    }
+}
+
+let sum: number = 0;
+
+for(let id of gameIds) {
+    sum += id;
+}
+
+console.log(sum)
--- 02/star2.ts
+++ 02/star2.ts
@@ -1,3 +1,84 @@
 import {getLines} from "../readFile";
 
-const lines = getLines("02/sample2.txt");
+const lines = getLines("02/input.txt");
+
+interface IGame {
+    id: number,
+    bags: IBag[]
+}
+
+interface IBag {
+    cubes: ICube[]
+}
+
+interface ICube {
+    amount: number,
+    color: string
+}
+
+let games: IGame[] = [];
+
+for(let line of lines) {
+    line = line.replace("Game", "");
+    const vals: string[] = line.split(":");
+
+    const sets = vals[1].split(";")
+
+    const bags: IBag[] = [];
+
+    for(const set of sets) {
+        let cubesRaw: string[] = set.split(",");
+        const cubes: ICube[] = [];
+        for(const cubeRaw of cubesRaw) {
+            const data = cubeRaw.trim().split(" ");
+            const cube: ICube = {
+                amount: parseInt(data[0]),
+                color: data[1]
+            }
+
+            cubes.push(cube);
+        }
+
+        const bag: IBag = {
+            cubes: cubes
+        }
+
+        bags.push(bag)
+    }
+
+    let game: IGame = {
+        id: parseInt(vals[0].trim()),
+        bags: bags
+    }
+
+    games.push(game);
+}
+
+let sumOfPows = 0;
+
+for(let game of games) {
+    let bags: IBag[] = game.bags;
+    let check: boolean = false;
+
+    let green: number = 0;
+    let red: number = 0;
+    let blue: number = 0;
+
+    for(let bag of bags) {
+        for(let cube of bag.cubes) {
+            if(cube.color === "green"&& cube.amount > green) {
+                green = cube.amount;
+            }
+            else if(cube.color === "blue"&& cube.amount > blue) {
+                blue = cube.amount;
+            }
+            else if(cube.color === "red" && cube.amount > red) {
+                red = cube.amount;
+            }
+        }
+    }
+
+    sumOfPows += (green * red * blue);
+}
+
+console.log(sumOfPows);
