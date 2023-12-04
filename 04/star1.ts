aae357af961b3ede48eb6685901ada2af9e0a113
Max B <m.bergmann@students.htl-leonding.ac.at>
2023-12-04 10:26:15 +0100

    added day04 star 1 and 2

--- 04/star1.ts
+++ 04/star1.ts
@@ -1,3 +1,60 @@
 import {getLines} from "../readFile";
 
 const lines: string[]  = getLines("04/sample1.txt");
+
+interface ICard {
+    id: number,
+    elfCards: number[],
+    myCards: number[]
+}
+
+const finalCards: ICard[] = [];
+
+for(let line of lines) {
+    const vals = line.split(":")
+    const id: number = parseInt(vals[0].replace("Card", "").trim());
+    const cards = vals[1].split("|");
+    const elfCards: number[] = [];
+    for(let card of cards[0].split(" ")) {
+        if(!isNaN(parseInt(card))) {
+            elfCards.push(parseInt(card));
+        }
+    }
+
+    const myCards: number[] = [];
+    for(let card of cards[1].split(" ")) {
+        if(!isNaN(parseInt(card))) {
+            myCards.push(parseInt(card));
+        }
+    }
+
+    let card: ICard = {
+        id,
+        elfCards,
+        myCards
+    };
+
+    finalCards.push(card);
+}
+
+let sum = 0;
+
+for(let cards of finalCards) {
+    sum += amountOfWinning(cards);
+}
+
+console.log(sum);
+
+
+function amountOfWinning(finalCard: ICard): number {
+    let cnt: number = 0
+    for(let card of finalCard.elfCards) {
+        if(finalCard.myCards.includes(card)) {
+            cnt++;
+        }
+    }
+
+    cnt--;
+    console.log(cnt)
+    return cnt < 0 ? 0 : Math.pow(2, cnt);
+}
--- 04/star2.ts
+++ 04/star2.ts
@@ -1,3 +1,81 @@
 import {getLines} from "../readFile";
 
-const lines: string[]  = getLines("04/sample2.txt");
+const lines: string[]  = getLines("04/input.txt");
+
+interface ICard {
+    id: number,
+    elfCards: number[],
+    myCards: number[]
+}
+
+const finalCards: ICard[] = [];
+
+for(let line of lines) {
+    const vals: string[] = line.split(":")
+    const id: number = parseInt(vals[0].replace("Card", "").trim());
+    const cards: string[] = vals[1].split("|");
+    const elfCards: number[] = [];
+    for(let card of cards[0].split(" ")) {
+        if(!isNaN(parseInt(card))) {
+            elfCards.push(parseInt(card));
+        }
+    }
+
+    const myCards: number[] = [];
+    for(let card of cards[1].split(" ")) {
+        if(!isNaN(parseInt(card))) {
+            myCards.push(parseInt(card));
+        }
+    }
+
+    let card: ICard = {
+        id,
+        elfCards,
+        myCards
+    };
+
+    finalCards.push(card);
+}
+
+
+
+function processScratchcards(cards: ICard[]): number {
+    let sets: number[] = [];
+    let winning: number[] = [];
+
+    for(let card of cards) {
+        sets.push(1);
+        winning.push(hasWinning(card));
+    }
+
+    for(let i: number = 0; i < cards.length; i++) {
+        for(let k: number  = 0; k < sets[i]; k++) {
+            for (let j: number  = i; j < i + winning[i]; j++) {
+                sets[j+1]++;
+            }
+        }
+
+        console.log(sets[i])
+    }
+
+    let sum: number = 0;
+    for(let set of sets) {
+        sum += set;
+    }
+
+    return sum;
+}
+
+function hasWinning(finalCard: ICard): number {
+    let cnt: number = 0
+    for(let card of finalCard.elfCards) {
+        if(finalCard.myCards.includes(card)) {
+            cnt++;
+        }
+    }
+
+    return cnt;
+}
+
+const result: number = processScratchcards(finalCards);
+console.log(result);
